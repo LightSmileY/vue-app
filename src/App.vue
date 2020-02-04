ed3e51b02f4cd72427ee484462451682a24c95b7
LightSmileY <2434740987@qq.com>
2020-02-04 14:42:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Index</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul, li {
+    list-style: none;
+  }
+</style>
